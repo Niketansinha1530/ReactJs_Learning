059414f9992e0a79280e0f18ef1df413745cbf29
Niketansinha1530 <niketansinha1@gmail.com>
2024-03-15 00:12:14 +0530

    learn second way to write context

--- 08miniContext/src/Components/Profile.jsx
+++ 08miniContext/src/Components/Profile.jsx
@@ -4,7 +4,12 @@ import UserContext from "../Context/UserContext";
 function Profile() {
   const { user } = useContext(UserContext);
   if (!user) return <div>Please Login</div>;
-  return <div>Welcome {user.username}</div>;
+  return (
+    <div>
+      Welcome {user.username}
+      {user.password}
+    </div>
+  );
 }
 
 export default Profile;
