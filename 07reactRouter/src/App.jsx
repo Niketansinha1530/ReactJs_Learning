4318ea8008479078600817a108c820359e09a67a
Niketansinha1530 <niketansinha1@gmail.com>
2024-03-19 11:08:41 +0530

    remove comments

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -4,13 +4,7 @@ import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <>
-      {/* <Header />
-      <Home />
-      <Footer /> */}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
